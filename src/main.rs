bbcb4e671031118e3e60b7c434d515b07f3beaee
vmarcella <cenz@cenz.io>
2022-09-09 22:22:08 -0700

    [add] 2D points, a metric space trait, and a distance test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+trait MetricSpace {
+  type Distance;
+  fn distance(&self, other: &Self) -> Self::Distance;
+}
+
+struct Point {
+  x: f64,
+  y: f64,
+}
+
+impl MetricSpace for Point {
+  type Distance = f64;
+
+  /// Compute the euclidean distance between two points in 2D space.
+  fn distance(&self, other: &Self) -> Self::Distance {
+    ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_distance() {
+    let p1 = Point { x: 0.0, y: 0.0 };
+    let p2 = Point { x: 3.0, y: 4.0 };
+    assert_eq!(p1.distance(&p2), 5.0);
+  }
 }
