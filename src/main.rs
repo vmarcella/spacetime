0d40038073ff3653e9330073221b4c4cc806b655
vmarcella <cenz@cenz.io>
2022-09-09 22:33:15 -0700

    [add] test for checking that the distance betweenpoints is linear.

--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,17 @@ mod tests {
   }
 
   #[test]
-  fn starting_point_doesnt_matter() {
+  fn distance_is_symmetric() {
     let p1 = Point { x: 0.0, y: 0.0 };
     let p2 = Point { x: 3.0, y: 4.0 };
     assert_eq!(p2.distance(&p1), p1.distance(&p2));
+  }
+
+  #[test]
+  fn distance_is_linear() {
+    let p1 = Point { x: 0.0, y: 0.0 };
+    let p2 = Point { x: 3.0, y: 4.0 };
+    let p3 = Point { x: 6.0, y: 8.0 };
+    assert_eq!(p1.distance(&p3), p1.distance(&p2) + p2.distance(&p3));
   }
 }
